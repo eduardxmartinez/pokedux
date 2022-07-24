26dbc89827f0b19d4d1e50cd023c6f1523f5f221
eduardxmartinez <eduardom17809@gmail.com>
2022-07-23 18:28:17 -0700

    ci=false

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,5 @@
 import { Input } from "antd";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setSearch } from "../slices/searchSlice";
 
 const Searcher = () => {
